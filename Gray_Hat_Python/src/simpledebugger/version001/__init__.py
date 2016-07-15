c971e9b4a9009e3000e8296ed40e2be95c3a0e6c
MrLi <MrLi@MrLi-PC>
2016-07-15 23:09:56 +0800

    load函数完成

--- Gray_Hat_Python/src/simpledebugger/version001/__init__.py
+++ Gray_Hat_Python/src/simpledebugger/version001/__init__.py
@@ -1,4 +1,7 @@
 '''
     This is version001
     about 程序最简单最粗糙的版本
+    my_debugger_defines.py # 数据结构
+    my_debugger.py # 功能
+    my_test_%.py # 测试
 '''
---
+++ Gray_Hat_Python/src/simpledebugger/version001/my_test_load.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+'''
+Created from 2016-07-15 23:04:38
+
+@author: MrLi
+@note: 测试function: debugger.load函数
+'''
+
+import my_debugger
+debugger = my_debugger.debugger()
+debugger.load('C:\\windows\\System32\\calc.exe')
+
